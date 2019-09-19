9814082e19292ca62db6ff2d39f972cbccef7a6d
40823242 <40823242@gm.nfu.edu.tw>
2019-09-19 21:14:14 +0800

    initial add

---
+++ content/tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages": [{'title': 'About', 'text': '\n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}, {'title': 'USB2', 'text': '', 'tags': '', 'url': 'USB2.html'}]};
